f5ff2bcee56c581a0c2fc5ac92a3317ef904c640
shenetworks <serena@shenetworks.tech>
2024-05-21 09:48:21 -0500

    Initial Commit

---
+++ StyleScribe/StyleScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleScribeApp.swift
+//  StyleScribe
+//
+//  Created by shenetworks on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
